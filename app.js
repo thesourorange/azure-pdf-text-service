dcb88a91a7e3222d94f2f95c37c16db875ec188c
thesourorange <nebrittl@microsoft.com>
2018-09-13 12:12:42 +1000

    New Version of Node

--- app.js
+++ app.js
@@ -8,24 +8,33 @@ var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 var pug = require('pug');
-
 var app = express();
+var favicon = require('serve-favicon');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var errorHandler = require('errorhandler')
+
+app.use(favicon(path.join(__dirname, 'public','images','favicon.ico'))); 
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
+app.use(logger('dev'));
+app.use(methodOverride());
+app.use(express.json());    
+
+if (process.env.NODE_ENV === 'development') {
+   app.use(errorhandler())
+}
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/modules", express.static(path.join(__dirname, 'node_modules')));
 
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+if (process.env.NODE_ENV === 'development') {
+  app.use(errorhandler())
 }
 
 function logMessage(message) {
